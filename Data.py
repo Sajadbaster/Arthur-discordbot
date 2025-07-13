f7633e4f93be9d5ccda05fa348dc16accb78c27b
Sajadbaster <94642551+Sajadbaster@users.noreply.github.com>
2025-07-14 00:07:03 +0300

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,61 @@
+import sqlite3
+
+connection=sqlite3.connect("Info.db")
+cursor=connection.cursor()
+cursor.execute("create table if not exists Bank(name text not null, money integer default 0, id interger primary key)")
+def Check(id):
+	cursor.execute("select id from Bank where id= ?", (id,))
+	check=cursor.fetchone()
+	return check
+	
+	
+class BankData:
+	def __init__(self, userID, name):
+		self.userID=userID
+		self.name=name
+	def create_bank(self):
+		check=Check(self.userID)
+		if check == None :
+			cursor.execute("insert into Bank values(?, ?, ?)", (self.name, 0, self.userID))
+			connection.commit()
+			return "Your bank account is created"
+		else:
+			return "You already have a bank account"
+			
+	def add_Money(self, addMoney):
+		check=Check(self.userID)
+		if check !=None:
+			cursor.execute("select money from Bank where id=(?)", (self.userID,))
+			beforeMoney=int(cursor.fetchone()[0])
+			total=beforeMoney+addMoney
+			cursor.execute("update Bank set money= ? where id=?", (total, self.userID))
+			connection.commit()
+			return f"You got: {beforeMoney+addMoney}"
+		else:
+			return "You don't have bank account"
+			
+	def transfer_Money(self, amount, sender_ID, receiver_ID):
+		check_sender=Check(sender_ID)
+		check_receiver=Check(receiver_ID)
+		if check_sender==None:
+			return f"<@{sender_ID}> You don't have a bank account", 2
+		if check_receiver==None:
+			return f"<@{receiver_ID}> don't have a bank account", 2
+		else:
+			cursor.execute("select money from Bank where id = ?", (sender_ID,))
+			sender_Money=cursor.fetchone()[0]
+			cursor.execute("select money from Bank where id = ?", (receiver_ID,))
+			receiver_Money=cursor.fetchone()[0]
+			if sender_Money >= amount:
+				take_money=sender_Money-amount
+				cursor.execute("update Bank set money= ? where id=?", (take_money, sender_ID))
+				total=receiver_Money+amount
+				cursor.execute("update Bank set money= ? where id=(?)", (total, receiver_ID))
+				connection.commit()
+				return (f"<@{receiver_ID}> got Money from <@{sender_ID}>", 0)
+			else:
+				return f"<@{sender_ID}> You don't have enough money", 1
+	def get_info(self):
+		cursor.execute("select * from Bank where id= ?", (self.userID,))
+		theData= cursor.fetchone()
+		return theData
--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
-import discord, json,logging, os, random, emoji
+import discord, json,logging, random, emoji
 from discord.ext import commands
 from discord import app_commands
 from weather import Weather
 from rps import rps
+from Data import BankData
 
 with open("Api.json", "r") as API:
 	APIs=json.load(API)["api"][0]
@@ -13,9 +14,10 @@ with open("Answers.json", "r") as answer:
 handler= logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 intents=discord.Intents.all()
 
-pending_requests={}
+ownerID=834856531693797438
 
 #events
+
 class myBot(commands.Bot):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
@@ -29,46 +31,43 @@ class myBot(commands.Bot):
 			
 			
 	async def on_message(self, message):
+		messages_responses={
+			"hi": "Hiiiii",
+			"hytale": "F",
+			"arthur": "What do you want?",
+			"im sad": "https://youtu.be/QuNhTLVgV2Y\n\nCome and let's be sad and cry together ",
+			"i'm sad": "https://youtu.be/QuNhTLVgV2Y\n\nCome and let's be sad and cry together",
+			"hi comrade": "Hello Comrade",
+			"fuck you": "Fuck you you piece of shit",
+			}
+
 		content= message.content.lower()
 		
 		if message.author==self.user or message.author.bot == True:
 			return
-			
-		elif content == "hi" :
-			await message.reply("HIIIIII")
-			
-		elif content == "hytale":
-			await message.reply("F")
-			
-		elif "technoblade" in message.content.lower():
-			await message.reply("TECHNOBLADE NEVER DIE")
-			
-		elif content=="hi comrade":
-			await message.reply("Hello Comrade")
-			
-		elif content=="im sad" or content=="i'm sad":
-			await message.reply("https://youtu.be/QuNhTLVgV2Y\n\nAfter you listen to this song go and kill yourself")
-			
-		elif content=="fuck you":
-			await message.reply("Fuck you you piece of shit")
-			
-			await self.process_commands(message)
+		elif content in messages_responses:
+			await message.reply(messages_responses[content])
+			content=content.split()
+		elif "technoblade" in content:
+			await message.reply("TECHNOBLADE NEVER DIES")
+		await self.process_commands(message)
 			
 	async def on_message_edit(self, before, after):
 		if after.author.bot==True:
 			return 
 		else:
-			await after.reply(f"hey I saw you edit that from \"{before.content}\" to \"{after.content}\" >:)")
+			await after.reply(f"Hey I saw you edit that from \"{before.content}\" to \"{after.content}\" >:)")
 			
 			
 bot= myBot(command_prefix="/", intents=intents)
 
-#button function
+#button function (will work on it later)
 
 
 
 
 #commands
+
 @bot.tree.command(name="botofanswers", description="Got a question but no answer? Don’t worry Arthur’s here! Ask now and get the answer you need!")
 async def botofanswers(interaction: discord.Interaction, question: str):
 	await interaction.response.defer()
@@ -76,8 +75,10 @@ async def botofanswers(interaction: discord.Interaction, question: str):
 	the_respond.add_field(name=f"{question}", value=random.choice(answers), inline=False)
 	await interaction.followup.send(embed=the_respond)
 	
+	
 @bot.tree.command(name="weather", description="Arthur know the weather of every city don't ask why now ask Arthor and he will give it to you")
 async def weathercommand(interaction: discord.Interaction, city: str):
+
 	await interaction.response.defer()
 	theWeather=Weather(city)
 	result=theWeather.weather()
@@ -87,21 +88,84 @@ async def weathercommand(interaction: discord.Interaction, city: str):
 		await interaction.followup.send(embed=the_respond)
 	else:
 		await interaction.followup.send(f"{city} is not a City please enter a City name", ephemeral=True)
+		
+	
 @bot.tree.command(name="rps", description="Rock paper scissors a classic game play against Arthur and see your luck")
 async def rpscommand(interaction: discord.Interaction, play: str):
 	await interaction.response.defer()
-	
+	bank_account=BankData(interaction.user.id, interaction.user.name)
 	call=rps(play)
 	result=call.function()
 	if result!= None:
 		embed=discord.Embed(title=result[0], description=f"{interaction.user.name.capitalize()}: {result[2]}\nArthur: {result[1]}")
+		embed.set_footer(text=f"You got {result[-1]}$")
+		bank_account.add_Money(result[-1])
 		await interaction.followup.send(embed=embed)
 	else:
 		await interaction.followup.send("Please enter a valid play (rock, paper, scissors)", ephemeral=True)
+		
 
-#@bot.tree.command(name="log", description="log")
-#async def log(interaction: discord.Interaction):
-#	await interaction.response.send_message(interaction)
+@bot.tree.command(name="rdice", description="Arthur have a magical dice let's use it!")
+async def rdice(interaction: discord.Interaction, min: int=1, max: int=6):
+	await interaction.response.defer()
+	await interaction.followup.send(f"{random.randint(min, max)} {emoji.emojize(":game_die:")}")
+	
+
+@bot.tree.command(name="mba", description="Arthur own's bank account and you can make one for free! How cool is that")
+async def bank(interaction: discord.Interaction):
+	await interaction.response.defer()
+	bank=BankData(interaction.user.id,interaction.user.name)
+	await interaction.followup.send(f"{interaction.user.mention} {bank.create_bank()}")
+		
+				
+@bot.tree.command(name="give-money", description="feel generous today? Then give money to people!")
+async def givemoney(interaction: discord.Interaction, amount: int, user: discord.User):
+	await interaction.response.defer()
+	if user.bot==False:
+		if interaction.user.id==user.id:
+				await interaction.followup.send("You can't give money to yourself ", ephemeral=True)
+				return
+		call=BankData(user.id, user.name)
+		output=call.transfer_Money(amount, interaction.user.id, user.id)
+		if output[1] ==0:
+			embed=discord.Embed(title=f"{amount}$ transferred", description=output[0])
+			await interaction.followup.send(embed=embed)
+		elif output[1]>0:
+			embed=discord.Embed(title="Something went wrong", description=output[0])
+			await interaction.followup.send(embed=embed, ephemeral=True)
+		else:
+			embed=discord.Embed(title="Something went wrong", description=output[0])
+			await interaction.followup.send(embed=embed, ephemeral=True)
+			
+			
+			
+@bot.tree.command(name="profile", description="Well it's obvious by the name see your profile")
+async def profile(interaction: discord.Interaction,user: discord.User=None):
+			await interaction.response.defer()
+			if user==None:
+				call=BankData(interaction.user.id, interaction.user.name)
+				info=call.get_info()
+				if info==None:
+					call.create_bank()
+				embed=discord.Embed(title=f"{interaction.user.name} profile", description=f"Name: {interaction.user.name}\n\nMoney: {info[1]}\n\nID: {interaction.user.id}")
+				embed.set_thumbnail(url=interaction.user.avatar)
+				await interaction.followup.send(embed=embed)
+			else:
+				if user.bot !=True:
+					call=BankData(user.id, user.name)
+					info=call.get_info()
+					if info==None:
+						call.create_bank()
+						info=call.get_info()
+					embed=discord.Embed(title=f"{user.name} profile", description=f"Name: {user.name}\n\nMoney: {info[1]}\n\nID: {user.id}")
+					embed.set_thumbnail(url=user.avatar)
+					await interaction.followup.send(embed=embed)
+				else:
+					await interaction.response.send("You can't check bot profile", ephemeral=True)
+				
+@bot.tree.command(name="log", description="log")
+async def log(interaction: discord.Interaction, user: discord.User):
+	await interaction.response.send_message(f"{interaction}\n\n{user.bot}")
 
 
 bot.run(discordToken)
